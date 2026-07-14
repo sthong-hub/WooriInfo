69072e32923bb9123f07929bdc26cbe57f8242c1
sthong-hub <sthong@smilelog.kr>
2026-07-14 15:39:26 +0900

    Update config.js
    Update config.js

---
+++ config.js
@@ -0,0 +1,11 @@
+@@ -67,8 +67,8 @@ const CONFIG = {
+   accounts: {
+     groom: [
+       { role: "신랑", name: "일이삼", bank: "국민은행", number: "123-00-045678" },
+-      { role: "아버지", name: "사오육", bank: "신한은행", number: "000-000-000000" },
+-      { role: "어머니", name: "칠팔구", bank: "우리은행", number: "000-000-000000" }
++      { role: "아버지", name: "구팔칠", bank: "농협은행", number: "222-01-213456" },
++      { role: "어머니", name: "육오사", bank: "농협은행", number: "333-02-056789" }
+     ],
+     bride: [
+       { role: "신부", name: "삼이일", bank: "국민은행", number: "456-03-123456" },
